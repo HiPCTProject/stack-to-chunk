5f314eedc66bfe0598b77d875750167cc1857664
David Stansby <dstansby@gmail.com>
2023-10-24 15:24:07 +0100

    Add some more docs

--- src/stack_to_chunk/main.py
+++ src/stack_to_chunk/main.py
@@ -85,7 +85,19 @@ def create_group(
     return root
 
 
-def copy_slab(arr_zarr: zarr.Array, slab: da.Array, zstart: int, zend: int) -> None:  # type: ignore[name-defined]
+def copy_slab(arr_zarr: zarr.Array, slab: da.Array, zstart: int, zend: int) -> None:
+    """
+    Copy a single slab of data to a zarr array.
+
+    Parameters
+    ----------
+    arr_zarr :
+        Array to copy to.
+    slab :
+        Slab of data to copy.
+    zstart, zend :
+        Start and end indices to copy to in destination array.
+    """
     print(f"Copying z={zstart} -> {zend}")
     # Write out data
     arr_zarr[:, :, zstart:zend] = np.array(slab)
--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+"""Quick test script."""
 from pathlib import Path
 
 import dask.array as da
@@ -17,7 +18,7 @@ if __name__ == "__main__":
     nslice = 256
     jp2_path = Path("/Users/dstansby/Dropbox (UCL)/A98/A98_jp2")
     jp2s = [glymur.Jp2k(p) for p in sorted(jp2_path.glob("*.jp2"))]
-    stack = da.stack([da.from_array(jp2) for jp2 in jp2s], axis=-1)
+    stack = da.stack([da.from_array(jp2) for jp2 in jp2s], axis=-1)  # type: ignore[attr-defined]
 
     slice_size_bytes = (
         stack.nbytes // stack.size * stack.chunksize[0] * stack.chunksize[1]
