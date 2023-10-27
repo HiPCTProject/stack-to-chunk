fa653c75aca9bfc8d2dd41da11d11e3dee8ecb6b
David Stansby <dstansby@gmail.com>
2023-10-27 12:21:21 +0100

    Refactor to use a class

--- src/stack_to_chunk/tests/test_main.py
+++ src/stack_to_chunk/tests/test_main.py
@@ -3,18 +3,18 @@ from pathlib import Path
 
 import dask.array as da
 import numpy as np
+import zarr
 
-from stack_to_chunk import copy_to_zarr, create_group
+from stack_to_chunk import MultiScaleGroup
 
 
 def test_workflow(tmp_path: Path) -> None:
     """Basic smoke test of the workflow as a user would use it."""
-    group = create_group(
+    group = MultiScaleGroup(
         tmp_path / "group.zarr",
         name="my_zarr_group",
         spatial_unit="centimeter",
         voxel_sizes=(3, 4, 5),
-        n_downsample_levels=2,
     )
 
     assert (tmp_path / "group.zarr").exists()
@@ -23,9 +23,17 @@ def test_workflow(tmp_path: Path) -> None:
     shape = (583, 245, 156)
     arr = da.random.randint(low=0, high=2**16, dtype=np.uint16, size=shape)
     arr = arr.rechunk(chunks=(shape[0], shape[1], 1))
-    copy_to_zarr(arr, group, n_processes=2, chunk_size=chunk_size, compressor="default")
+    group.add_full_res_data(
+        arr,
+        n_processes=2,
+        chunk_size=chunk_size,
+        compressor="default",
+    )
 
-    assert "0" in group
-    assert group["0"].chunks == (chunk_size, chunk_size, chunk_size)
-    assert group["0"].shape == shape
-    assert group["0"].dtype == np.uint16
+    zarr_arr = zarr.open(tmp_path / "group.zarr" / "0")
+    assert zarr_arr.chunks == (chunk_size, chunk_size, chunk_size)
+    assert zarr_arr.shape == shape
+    assert zarr_arr.dtype == np.uint16
+
+    # Check that data is equal in dask array and zarr array
+    np.testing.assert_equal(arr[:], zarr_arr[:])
