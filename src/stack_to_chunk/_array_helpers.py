1110ffef180bacdcbbac8f081e47a90eabdced93
David Stansby <dstansby@gmail.com>
2024-04-23 15:31:54 +0100

    Fix messages

--- src/stack_to_chunk/_array_helpers.py
+++ src/stack_to_chunk/_array_helpers.py
@@ -18,13 +18,13 @@ def _copy_slab(arr_zarr: zarr.Array, slab: da.Array, zstart: int, zend: int) -> 
         Start and end indices to copy to in destination array.
 
     """
-    logger.info(f"Reading z={zstart} -> {zend}")
+    logger.info(f"Reading z={zstart} -> {zend-1}")
     data = np.empty(slab.shape, dtype=slab.dtype)
     for i in range(slab.shape[2]):
-        logger.info(f"Reading {zstart + i}")
+        logger.info(f"Reading z={zstart + i}")
         data[:, :, i] = np.array(slab[:, :, i])
 
-    logger.info(f"Writing z={zstart} -> {zend}")
+    logger.info(f"Writing z={zstart} -> {zend-1}")
     # Write out data
     arr_zarr[:, :, zstart:zend] = data
-    logger.info(f"Finished copying z={zstart} -> {zend}")
+    logger.info(f"Finished copying z={zstart} -> {zend-1}")
