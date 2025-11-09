ac3be711f12cd8e8ac7594a0951975e4c46914ed
Matthieu Chourrout <83714683+chourroutm@users.noreply.github.com>
2025-11-09 16:43:31 +0000

    Change verbose level of parallel processes

--- src/stack_to_chunk/main.py
+++ src/stack_to_chunk/main.py
@@ -402,7 +402,7 @@ class MultiScaleGroup:
 
         jobs = [_downsample_block(*args) for args in all_args]
         logger.info(f"Launching {len(jobs)} jobs")
-        Parallel(n_jobs=n_processes, verbose=10)(jobs)
+        Parallel(n_jobs=n_processes)(jobs)
 
         self._add_level_metadata(level)
         blosc.use_threads = blosc_use_threads
